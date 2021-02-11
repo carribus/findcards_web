6ca52e835b89c2b5e4d3149420e0ab25e9815405
Peter Mares <carribus@gmail.com>
2021-02-11 17:30:47 +0100

    Fixed server host

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const HOST = "find-cards.com/api"; 
-const HOST = "localhost:8000";
+const HOST = "find-cards.com/api"; 
+// const HOST = "localhost:8000";
 
 const API_SEARCH = `http://${HOST}/search?key=`;
 const API_VERSION = `http://${HOST}/version`;
